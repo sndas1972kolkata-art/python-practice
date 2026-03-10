9d8927623072a7cfce162023f3c6bb152e30ca78
sndas1972kolkata-art <sndas1972kolkata@gmail.com>
2026-03-10 15:42:58 +0530

    Add basic number checks and voting eligibility

---
+++ Day 4.py
@@ -0,0 +1,19 @@
+#Positive/Negative
+num = int(input("Enter number: "))
+if(num>0):
+    print("Positive")
+else:
+    print("Negative")
+#Largest of two numbers
+a=int(input("Enter the first the number : "))
+b=int(input("Enter the second number : "))
+if(a>b):
+    print(a,"is largest.")
+else:
+    print(b,"is largest.")
+#Voting eligibility
+age = int(input("Enter your age:"))
+if(age>=18):
+    print("Eligible")
+else:
+    print("Not Eligible")
