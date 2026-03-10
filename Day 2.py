8b206acf229cfcc47438724ad351099f69aac993
sndas1972kolkata-art <sndas1972kolkata@gmail.com>
2026-03-10 15:40:03 +0530

    Add greeting and area calculation functionality

---
+++ Day 2.py
@@ -0,0 +1,15 @@
+# Greet
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello", name)
+print("You are", age, "years old")
+# Add
+a=int(input("Enter the first number : "))
+b=int(input("Enter the second number : "))
+Sum=a+b
+print("The sum is ",Sum)
+#Area
+l=int(input("Enter the length : "))
+b=int(input("Enter the breath : "))
+Area=l*b
+print("The Area of rectangle is ",Area)
